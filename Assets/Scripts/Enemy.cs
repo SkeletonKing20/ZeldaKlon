da08b835726cff0161cc1dde3288c95e55c1ab9a
SkeletonKing20 <n.bonn2002@gmail.com>
2021-03-15 09:17:43 +0100

    IDamageable und Enemy

---
+++ Assets/Scripts/Enemy.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour, IDamageable
+{
+    int currentHP;
+    public int initialHP = 3;
+
+    private void Awake()
+    {
+        currentHP = initialHP;
+    }
+    public void TakeDamage()
+    {
+        currentHP--;
+
+        if (currentHP <= 0)
+        {
+            Die();
+        }
+    }
+
+    private void Die()
+    {
+        Destroy(gameObject);
+    }
+}
